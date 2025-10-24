658129d5be6119b2d6c336309e5f2e6f8957b6e8
Diego Riv Loz <siniestropix55@gmail.com>
2025-10-24 09:10:45 -0600

    agregar producto a base de datos

--- index.js
+++ index.js
@@ -2,11 +2,63 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.use(express.json());
+//motor de vistas
+app.set('view engine', 'ejs');
 
-app.get('/inicio', (req, res) => {
-    res.send('¡Hola mundo!');
+//express
+const admin = require("firebase-admin");
+const cors = require("cors");
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+// credenciales
+const serviceAccount = require("./firebase_key.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();// Rutas de la API de productos
+
+//Indica que inicie una pagina con los siguientes textos
+app.get("/productos", async (req, res) => {
+    try {
+        const items = await db.collection("productos").get();
+        const productos = items.docs.map(doc => {
+            const data = doc.data();
+            return {
+                id: doc.id,
+                nombre:data.nombre,
+                descripcion: data.descripcion,
+                precio: data.precio,
+                imagen: data.imagen
+            };
+        });
+        res.render('inicio', {productos});
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+});
+
+app.get('/productos/add', (req, res) => {
+res.render('form', { producto: null, nombre: 'Crear Producto'});
 });
 
 app.listen(port, () => {
     console.log('Servidor inicializado en http://localhost:'+ port);
+});
+
+app.post('/productos', async (req, res) => {
+try {
+    const {nombre, precio, descripcion, imagen } = req.body;
+    const nuevo = {
+    nombre: nombre || '',
+    precio: parseFloat(precio) || 0,
+    descripcion,
+    imagen: imagen || ''
+};
+await db.collection('productos').add(nuevo);
+res.redirect('/productos');
+} catch (err) {
+    console.error(err);
+    res.status(500).send('Error al crear producto');
+}
 });
